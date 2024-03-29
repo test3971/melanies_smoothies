a89757e99859ae4a4c025961a18e0a3215ffebce
test3971 <165368471+test3971@users.noreply.github.com>
2024-03-29 17:13:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,10 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write("Name on Smoothie will be: ", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
 
 pd_df = my_dataframe.to_pandas()
 
-ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe)
+ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections=5)
 
 if ingredients_list:
     ingredients_string = ''
