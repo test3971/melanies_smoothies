d689d813b141f5d84ff060c3abb7aba4f8c2f6e7
test3971 <165368471+test3971@users.noreply.github.com>
2024-03-29 17:08:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,33 +14,31 @@ st.write(
     """
 )
 
-# option = st.selectbox(
-#     'What is your favourite fruit?',
-#     ('Banana', 'Strawberry', 'Peaches'))
-
-# st.write('Your favourite fruit is:', option)
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("Name on Smoothie will be: ", name_on_order)
 
-
-#session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
-ingredients_list = st.multiselect(
-    'Choose upto 5 ingredients:'
-, my_dataframe)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
+ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe)
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
